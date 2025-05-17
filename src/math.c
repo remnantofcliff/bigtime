9fddcc2202ae619bbb59b307d8400dad9a5e7cfb
remnantofcliff <ville.nieminen0@gmail.com>
2025-05-17 23:40:33 +0300

    Fix length calculation + add more functions

--- src/math.c
+++ src/math.c
@@ -1,5 +1,8 @@
 #include "math.h"
 #include <SDL3/SDL_stdinc.h>
+#include <math.h>
+
+float bt_recip(float x) { return 1.0f / x; }
 
 struct bt_vec3 bt_cross_product(struct bt_vec3 a, struct bt_vec3 b) {
   return (struct bt_vec3){{
@@ -9,44 +12,168 @@ struct bt_vec3 bt_cross_product(struct bt_vec3 a, struct bt_vec3 b) {
   }};
 }
 
-struct bt_vec3 bt_vec3_divf(struct bt_vec3 a, float b) {
-  struct bt_vec3 result = {};
-  for (size_t i = 0; i < 3; ++i) {
-    result.arr[i] = a.arr[i] / b;
+void bt_look_to(struct bt_mat4 out[restrict static 1],
+                struct bt_vec3 const eye[static 1],
+                struct bt_vec3 const dir[static 1],
+                struct bt_vec3 const up[static 1]) {
+  struct bt_vec3 f = bt_vec3_negate(*dir);
+  struct bt_vec3 s = bt_vec3_normalize(bt_cross_product(f, *up));
+  struct bt_vec3 u = bt_cross_product(s, f);
+  out->arr[0] = s.arr[0];
+  out->arr[1] = u.arr[0];
+  out->arr[2] = -f.arr[0];
+  out->arr[3] = 0.0f;
+  out->arr[4] = s.arr[1];
+  out->arr[5] = u.arr[1];
+  out->arr[6] = -f.arr[1];
+  out->arr[7] = 0.0f;
+  out->arr[8] = s.arr[2];
+  out->arr[9] = u.arr[2];
+  out->arr[10] = -f.arr[2];
+  out->arr[11] = 0.0f;
+  out->arr[12] = -bt_vec3_dot(*eye, s);
+  out->arr[13] = -bt_vec3_dot(*eye, u);
+  out->arr[14] = bt_vec3_dot(*eye, f);
+  out->arr[15] = 1.0f;
+}
+
+void bt_perspective(struct bt_mat4 out[static 1], float fovy,
+                    float aspect_ratio, float near, float far) {
+  float sin_fov = SDL_sinf(0.5f * fovy);
+  float cos_fov = SDL_cosf(0.5f * fovy);
+  float h = cos_fov / sin_fov;
+  float w = h / aspect_ratio;
+  float r = far / (far - near);
+  out->arr[0] = w;
+  out->arr[1] = 0.0f;
+  out->arr[2] = 0.0f;
+  out->arr[3] = 0.0f;
+  out->arr[4] = 0.0f;
+  out->arr[5] = h;
+  out->arr[6] = 0.0f;
+  out->arr[7] = 0.0f;
+  out->arr[8] = 0.0f;
+  out->arr[9] = 0.0f;
+  out->arr[10] = r;
+  out->arr[11] = 1.0f;
+  out->arr[12] = 0.0f;
+  out->arr[13] = 0.0f;
+  out->arr[14] = -r * near;
+  out->arr[15] = 0.0f;
+}
+
+float bt_vec2_length(struct bt_vec2 a) {
+  return SDL_sqrtf(bt_vec2_sum(bt_vec2_mul(a, a)));
+}
+
+struct bt_vec2 bt_vec2_mul(struct bt_vec2 a, struct bt_vec2 b) {
+  typeof(a) result = {};
+  for (size_t i = 0; i < SDL_arraysize(a.arr); ++i) {
+    result.arr[i] = a.arr[i] * b.arr[i];
   }
   return result;
+}
+
+struct bt_vec2 bt_vec2_mulf(struct bt_vec2 a, float b) {
+  typeof(a) result = {};
+  for (size_t i = 0; i < SDL_arraysize(a.arr); ++i) {
+    result.arr[i] = a.arr[i] * b;
+  }
+  return result;
+}
+
+struct bt_vec2 bt_vec2_normalize(struct bt_vec2 a) {
+  return bt_vec2_mulf(a, bt_recip(bt_vec2_length(a)));
+}
+
+struct bt_vec2 bt_vec2_normalize_or_zero(struct bt_vec2 a) {
+  float recip_len = bt_recip(bt_vec2_length(a));
+  if (!isfinite(recip_len) && recip_len > 0.0f) {
+    return (typeof(a)){};
+  }
+
+  return bt_vec2_mulf(a, recip_len);
+}
+
+float bt_vec2_sum(struct bt_vec2 a) {
+  float sum = 0.0f;
+  for (size_t i = 0; i < SDL_arraysize(a.arr); ++i) {
+    sum += a.arr[i];
+  }
+  return sum;
 }
 
 float bt_vec3_dot(struct bt_vec3 a, struct bt_vec3 b) {
-  return a.arr[0] * b.arr[0] + a.arr[1] * b.arr[1] + a.arr[2] * b.arr[2];
+  return bt_vec3_sum(bt_vec3_mul(a, b));
 }
 
-float bt_vec3_length(struct bt_vec3 x) {
-  return SDL_sqrtf(x.arr[0] + x.arr[1] + x.arr[2]);
+float bt_vec3_length(struct bt_vec3 a) {
+  return SDL_sqrtf(bt_vec3_sum(bt_vec3_mul(a, a)));
 }
 
-struct bt_vec3 bt_vec3_negate(struct bt_vec3 x) {
-  struct bt_vec3 result = {};
-  for (size_t i = 0; i < 3; ++i) {
-    result.arr[i] = -x.arr[i];
+struct bt_vec3 bt_vec3_lerp(struct bt_vec3 a, struct bt_vec3 b, float t) {
+  typeof(a) result = {};
+  for (size_t i = 0; i < SDL_arraysize(a.arr); ++i) {
+    result.arr[i] = (b.arr[i] - a.arr[i]) * t + a.arr[i];
   }
   return result;
 }
 
-struct bt_vec3 bt_vec3_normalize(struct bt_vec3 x) {
-  return bt_vec3_divf(x, bt_vec3_length(x));
+struct bt_vec3 bt_vec3_mul(struct bt_vec3 a, struct bt_vec3 b) {
+  typeof(a) result = {};
+  for (size_t i = 0; i < SDL_arraysize(a.arr); ++i) {
+    result.arr[i] = a.arr[i] * b.arr[i];
+  }
+  return result;
 }
 
-struct bt_vec4 bt_vec4_add_vec4(struct bt_vec4 a, struct bt_vec4 b) {
-  struct bt_vec4 result = {};
+struct bt_vec3 bt_vec3_mulf(struct bt_vec3 a, float b) {
+  typeof(a) result = {};
+  for (size_t i = 0; i < SDL_arraysize(a.arr); ++i) {
+    result.arr[i] = a.arr[i] * b;
+  }
+  return result;
+}
+
+struct bt_vec3 bt_vec3_negate(struct bt_vec3 a) {
+  typeof(a) result = {};
+  for (size_t i = 0; i < SDL_arraysize(a.arr); ++i) {
+    result.arr[i] = -a.arr[i];
+  }
+  return result;
+}
+
+struct bt_vec3 bt_vec3_normalize(struct bt_vec3 a) {
+  return bt_vec3_mulf(a, bt_recip(bt_vec3_length(a)));
+}
+
+struct bt_vec3 bt_vec3_normalize_or_zero(struct bt_vec3 a) {
+  float recip_len = bt_recip(bt_vec3_length(a));
+  if (isfinite(recip_len) && recip_len > 0.0f) {
+    return (typeof(a)){};
+  }
+
+  return bt_vec3_mulf(a, recip_len);
+}
+
+float bt_vec3_sum(struct bt_vec3 a) {
+  float sum = 0.0f;
+  for (size_t i = 0; i < SDL_arraysize(a.arr); ++i) {
+    sum += a.arr[i];
+  }
+  return sum;
+}
+
+struct bt_vec4 bt_vec4_add(struct bt_vec4 a, struct bt_vec4 b) {
+  typeof(a) result = {};
   for (size_t i = 0; i < 4; ++i) {
     result.arr[i] = a.arr[i] + b.arr[i];
   }
   return result;
 }
 
-struct bt_vec4 bt_vec4_mul_vec4(struct bt_vec4 a, struct bt_vec4 b) {
-  struct bt_vec4 result = {};
+struct bt_vec4 bt_vec4_mul(struct bt_vec4 a, struct bt_vec4 b) {
+  typeof(a) result = {};
   for (size_t i = 0; i < 4; ++i) {
     result.arr[i] = a.arr[i] * b.arr[i];
   }
@@ -80,14 +207,14 @@ void bt_mat4_mul_vec4(struct bt_mat4 const in1[static 1],
   bt_mat4_into_columns(in1, in1_cols);
 
   for (size_t i = 0; i < 4; ++i) {
-    *out = bt_vec4_add_vec4(
-        *out, bt_vec4_mul_vec4(in1_cols[i], bt_vec4_splat(in2->arr[i])));
+    *out =
+        bt_vec4_add(*out, bt_vec4_mul(in1_cols[i], bt_vec4_splat(in2->arr[i])));
   }
 }
 
-void bt_mat4_mul_mat4(struct bt_mat4 const in1[static 1],
-                      struct bt_mat4 const in2[static 1],
-                      struct bt_mat4 out[restrict static 1]) {
+void bt_mat4_mul(struct bt_mat4 const in1[static 1],
+                 struct bt_mat4 const in2[static 1],
+                 struct bt_mat4 out[restrict static 1]) {
   struct bt_vec4 in2_cols[4] = {};
   bt_mat4_into_columns(in2, in2_cols);
 
@@ -101,54 +228,4 @@ void bt_mat4_mul_mat4(struct bt_mat4 const in1[static 1],
       out->arr[i * 4 + j] = res[i].arr[j];
     }
   }
-}
-
-void bt_look_to(struct bt_mat4 out[restrict static 1],
-                     struct bt_vec3 const eye[static 1],
-                     struct bt_vec3 const dir[static 1],
-                     struct bt_vec3 const up[static 1]) {
-  struct bt_vec3 f = bt_vec3_negate(*dir);
-  struct bt_vec3 s = bt_vec3_normalize(bt_cross_product(f, *up));
-  struct bt_vec3 u = bt_cross_product(s, f);
-  out->arr[0] = s.arr[0];
-  out->arr[1] = u.arr[0];
-  out->arr[2] = -f.arr[0];
-  out->arr[3] = 0.0f;
-  out->arr[4] = s.arr[1];
-  out->arr[5] = u.arr[1];
-  out->arr[6] = -f.arr[1];
-  out->arr[7] = 0.0f;
-  out->arr[8] = s.arr[2];
-  out->arr[9] = u.arr[2];
-  out->arr[10] = -f.arr[2];
-  out->arr[11] = 0.0f;
-  out->arr[12] = -bt_vec3_dot(*eye, s);
-  out->arr[13] = -bt_vec3_dot(*eye, u);
-  out->arr[14] = bt_vec3_dot(*eye, f);
-  out->arr[15] = 1.0f;
-}
-
-void bt_perspective(struct bt_mat4 out[static 1], float fovy,
-                         float aspect_ratio, float near, float far) {
-  float sin_fov = SDL_sinf(0.5f * fovy);
-  float cos_fov = SDL_cosf(0.5f * fovy);
-  float h = cos_fov / sin_fov;
-  float w = h / aspect_ratio;
-  float r = far / (far - near);
-  out->arr[0] = w;
-  out->arr[1] = 0.0f;
-  out->arr[2] = 0.0f;
-  out->arr[3] = 0.0f;
-  out->arr[4] = 0.0f;
-  out->arr[5] = h;
-  out->arr[6] = 0.0f;
-  out->arr[7] = 0.0f;
-  out->arr[8] = 0.0f;
-  out->arr[9] = 0.0f;
-  out->arr[10] = r;
-  out->arr[11] = 1.0f;
-  out->arr[12] = 0.0f;
-  out->arr[13] = 0.0f;
-  out->arr[14] = -r * near;
-  out->arr[15] = 0.0f;
 }
--- src/math.h
+++ src/math.h
@@ -5,6 +5,10 @@
 #ifndef BT_MATH_H
 #define BT_MATH_H
 
+struct bt_vec2 {
+  float arr[2];
+};
+
 struct bt_vec3 {
   float arr[3];
 };
@@ -13,6 +17,9 @@ struct bt_vec4 {
   float arr[4];
 };
 
+/*
+ * A 4x4 matrix with column-major order
+ */
 struct bt_mat4 {
   float arr[16];
 };
@@ -27,41 +34,53 @@ constexpr struct bt_mat4 bt_mat4_identity = {
     },
 };
 
+float bt_recip(float x);
 struct bt_vec3 bt_cross_product(struct bt_vec3 a, struct bt_vec3 b);
-
-// Normal operations
-
-struct bt_vec3 bt_vec3_divf(struct bt_vec3 a, float b);
-float bt_vec3_dot(struct bt_vec3 a, struct bt_vec3 b);
-float bt_vec3_length(struct bt_vec3 x);
-struct bt_vec3 bt_vec3_lerp(struct bt_vec3 a, struct bt_vec3 b, float t);
-struct bt_vec3 bt_vec3_negate(struct bt_vec3 x);
-struct bt_vec3 bt_vec3_normalize(struct bt_vec3 x);
-struct bt_vec4 bt_vec4_add_vec4(struct bt_vec4 a, struct bt_vec4 b);
-struct bt_vec4 bt_vec4_mul_vec4(struct bt_vec4 a, struct bt_vec4 b);
-struct bt_vec4 bt_vec4_splat(float x);
-void bt_mat4_into_columns(struct bt_mat4 const in[static 1],
-                          struct bt_vec4 out[restrict static 4]);
-void bt_mat4_mul_vec4(struct bt_mat4 const in1[static 1],
-                      struct bt_vec4 const in2[static 1],
-                      struct bt_vec4 out[restrict static 1]);
-void bt_mat4_mul_mat4(struct bt_mat4 const in1[static 1],
-                      struct bt_mat4 const in2[static 1],
-                      struct bt_mat4 out[restrict static 1]);
-
-// Special operations
-
 /*
- * Left handed
+ * Computes a left-handed view matrix that looks from `eye` to direc  `dir`.
+ * `dir` must be normalized
  */
 void bt_look_to(struct bt_mat4 out[restrict static 1],
                 struct bt_vec3 const eye[static 1],
                 struct bt_vec3 const dir[static 1],
                 struct bt_vec3 const up[static 1]);
 /*
- * Left handed
+ * Computes a left-handed projection matrix using the provided values
  */
 void bt_perspective(struct bt_mat4 out[static 1], float fovy,
                     float aspect_ratio, float near, float far);
+
+float bt_vec2_length(struct bt_vec2 a);
+struct bt_vec2 bt_vec2_mul(struct bt_vec2 a, struct bt_vec2 b);
+struct bt_vec2 bt_vec2_mulf(struct bt_vec2 a, float b);
+/*
+ * Length of a needs to be > 0.0f or you will get garbage results
+ */
+struct bt_vec2 bt_vec2_normalize(struct bt_vec2 a);
+struct bt_vec2 bt_vec2_normalize_or_zero(struct bt_vec2 a);
+float bt_vec2_sum(struct bt_vec2 a);
+
+float bt_vec3_dot(struct bt_vec3 a, struct bt_vec3 b);
+float bt_vec3_length(struct bt_vec3 a);
+struct bt_vec3 bt_vec3_lerp(struct bt_vec3 a, struct bt_vec3 b, float t);
+struct bt_vec3 bt_vec3_mul(struct bt_vec3 a, struct bt_vec3 b);
+struct bt_vec3 bt_vec3_mulf(struct bt_vec3 a, float b);
+struct bt_vec3 bt_vec3_negate(struct bt_vec3 a);
+struct bt_vec3 bt_vec3_normalize(struct bt_vec3 a);
+struct bt_vec3 bt_vec3_normalize_or_zero(struct bt_vec3 a);
+float bt_vec3_sum(struct bt_vec3 a);
+
+struct bt_vec4 bt_vec4_add(struct bt_vec4 a, struct bt_vec4 b);
+struct bt_vec4 bt_vec4_mul(struct bt_vec4 a, struct bt_vec4 b);
+struct bt_vec4 bt_vec4_splat(float x);
+
+void bt_mat4_into_columns(struct bt_mat4 const in[static 1],
+                          struct bt_vec4 out[restrict static 4]);
+void bt_mat4_mul_vec4(struct bt_mat4 const in1[static 1],
+                      struct bt_vec4 const in2[static 1],
+                      struct bt_vec4 out[restrict static 1]);
+void bt_mat4_mul(struct bt_mat4 const in1[static 1],
+                 struct bt_mat4 const in2[static 1],
+                 struct bt_mat4 out[restrict static 1]);
 
 #endif
